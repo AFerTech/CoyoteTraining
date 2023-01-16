daee874ef6af2cc3e423cf7e1caa1e8fcb58eda9
Alexis Fernando <alexis96fer@gmail.com>
2023-01-16 16:03:08 -0600

    primer commit

---
+++ controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controllers;
+
+
+class LoginController
+{
+    public static function login(){
+        echo "Desde login";
+    }
+
+    public static function logout(){
+        echo "Desde logout";
+    }
+    public static function recuperar(){
+        echo "Desde recuperar";
+    }
+    public static function recuperado(){
+        echo "Desde recuperado";
+    }
+
+    public static function crear(){
+        echo "Desde crear cuenta";
+    }
+    
+}
