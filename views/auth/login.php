c8a90b4904dcdf9579ac2990ef4fe41a9d723f54
Alexis Fernando <alexis96fer@gmail.com>
2023-01-17 13:51:31 -0600

    formulario y enlaces para las cuentas

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,2 +1,32 @@
 <h1 class="pag-name">Login</h1>
 <p class="desc-pag">Iniciar Sesión</p>
+
+<form action="/" method="POST" class="form">
+    <div class="campo">
+        <label for="email">Correo Electronico</label>
+        <input 
+            type="email"
+            id="email"
+            placeholder="Correo Electronico"
+            name="email"
+        />
+    </div>  <!--Correo--> 
+
+    <div class="campo">
+        <label for="password">Contraseña</label>
+        <input 
+            type="password"
+            id="password"
+            placeholder="Contraseña"
+            name="password"
+            />
+    </div>
+
+    <input type="submit" class="btn" value="Iniciar Sesión">
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">Crear una cuenta</a>
+    <a href="/recuperar">Recuperar contraseña</a>
+
+</div>
