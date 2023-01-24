1da87c147f650e8cba90e4de067d261d35458bc6
Alexis Fernando <alexis96fer@gmail.com>
2023-01-24 13:04:34 -0600

    tabs y estilos para la navegacion

--- views/usuario/index.php
+++ views/usuario/index.php
@@ -2,6 +2,12 @@
 <p class="desc-pag">Elige tus servicios</p>
 
 <div id="app">
+    <nav class="tabs">
+        <button class="actual" type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="2">Cita</button>
+        <button type="button" data-paso="3">Resumen</button>
+
+    </nav>
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
         <p class="text-center">Elige tus servicios</p>
