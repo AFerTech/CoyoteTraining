c523e740f2a8acd1bcd94f08696573a77d5a8b9a
Alexis Fernando <alexis96fer@gmail.com>
2023-01-24 12:00:30 -0600

    router,controller y vista para las citas

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\CitaController;
 use Controllers\LoginController;
 
 $router = new Router();
@@ -27,6 +28,8 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
 
+// Usuarios
+$router->get('/cita', [CitaController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
---
+++ views/usuario/index.php
@@ -0,0 +1,2 @@
+
+<h1 class="pag-name">Citas</h1>
