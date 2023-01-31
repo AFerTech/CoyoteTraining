d8aaeeb152085d0f3c771f395ef164280a025552
Alexis Fernando <alexis96fer@gmail.com>
2023-01-30 21:42:40 -0600

    terminando el modelo CitaServicio

--- models/CitaServicio.php
+++ models/CitaServicio.php
@@ -7,5 +7,18 @@ use Model\ActiveRecord;
 
 
 class CitaServicio extends ActiveRecord{
-    
+
+    protected static $tabla = 'citasservicios';
+    protected $columnasDB = ['id', 'cita_id', 'servicio_id'];
+
+    public $id;
+    public $cita_id;
+    public $servicio_id;
+
+    public function __construct($args = []){
+        $this->$id = $args['id'] ?? null;
+        $this->$cita_id = $args['cita_id'] ?? '';
+        $this->servicio_id = $args['servicio_id'] ?? '';
+    }
+
 }
