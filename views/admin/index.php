84461f139c6c78aed52e1a98e294bb1d14dfbf60
Alexis Fernando <alexis96fer@gmail.com>
2023-02-02 13:17:04 -0600

    iniciando con el panel del admin

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,3 +1,17 @@
 <?php
     include_once __DIR__ . '/../templates/barra.php'
 ?>
+
+<h2>Buscar citas</h2>
+<div class="busqueda">
+    <form class="formulario" action="">
+        <label for="fecha">Fecha</label>
+        <div class="campo">
+            <input 
+            type="date"
+            id="fecha"
+            name="fecha">
+        </div>
+    </form>
+</div>
+<div id="citas-admin">Citas</div>
